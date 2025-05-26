b5492fb801754024fb5dfcfe386fc949935b9db6
GOURAV KUMAR BEHERA <85735841+Gourav56-rgb@users.noreply.github.com>
2025-05-26 19:58:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,9 +17,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors())
-
-app.options('*', cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
