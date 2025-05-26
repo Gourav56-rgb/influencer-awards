55fc9feae6273a638f932e8029a3b501206ee818
GOURAV KUMAR BEHERA <sourav7712389360@gmail.com>
2025-05-26 18:14:36 +0530

    final

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config()
 connectDB()
 
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
